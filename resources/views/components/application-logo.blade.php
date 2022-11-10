a81b366556ddbfef3a76b56734a9e8e9fdd9f770
sarifjaman <sarif.jaman111@gmail.com>
2022-11-11 00:37:11 +0600

    Laravel Breeze Authentication

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,7 @@
+<style type="text/css">
+    .image-circle {
+        border-radius: 50%;
+    }
+</style>
+
+<img src="{{ asset('logo/logo1.jpeg')}}" width="100px" alt="image" class="image-circle"/>
